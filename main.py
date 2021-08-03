aef4408a6b8a35bd7ef51fb82be885a565248d10
Fiifi Krampah <fiifi.krampah@gmail.com>
2021-08-02 22:35:41 -0500

    Prepare for v1.1.0.0 release

--- main.py
+++ main.py
@@ -1,18 +1,41 @@
+"""
+Simple program to compute the collatz conjecture of a given number.
+"""
+# Store the collatz numbers in a list
+steps = []
+
+
+# Function to compute the collatz conjecture for a given number
 def collatz(num):
+    global steps
     # if num is even
-    while (num > 1):
-        if num % 2 == 0:
-            num = num / 2 
-            print(num)
-        # else num is odd
-        else:
-            num = (3 * num) + 1
-            print(num)
+    if num % 2 == 0:
+        num = int(num / 2)
+        steps.append(num)
+        collatz(num)
+    # else if num is odd and not 1
+    elif (num % 2 == 1) and (num != 1):
+        num = int((3 * num) + 1)
+        steps.append(num)
+        collatz(num)
+    else:
+        # end if num reaches 1
+        pass
+    # return the number of steps
+    return(len(steps))
 
-collatz(10)
 
-# Eddy complete the following code here:
+def main():
+    name = input("What is your name?")
+    print("Welcome " + name)
 
-def mainFunc():
-    print("What is your name?") 
-    print("My name is Fiifi") # Change this to your name
+    # Call the collatz function with a value entered by the user
+    number = input("Enter a positive number: ")
+    result = collatz(int(number))
+    print(f"The collatz conjecture for the number you entered took: \
+        {result} calculations")
+    print(f"The collatz list is: {steps}")
+
+
+# Run program
+main()
