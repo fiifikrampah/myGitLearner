8a5bc3313c8ddbd79c60dda87642006fd120037e
Fiifi Krampah <fiifi.krampah@gmail.com>
2022-07-01 23:03:58 -0500

    [GH-#8] Implement the positive number check

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ Simple program to compute the collatz conjecture of a given number.
 # Store the collatz numbers in a list
 steps = []
 
+# The app should only take positive numbers
+ 
 
 # Function to compute the collatz conjecture for a given number
 def collatz(num):
