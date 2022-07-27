4714d04fb172c0e48c4bd2eaef0f6bd2be98ecd9
Fiifi Krampah <fiifi.krampah@gmail.com>
2022-07-26 20:46:42 -0500

    Improve the logic such that the runtime is now 3s

--- main.py
+++ main.py
@@ -7,23 +7,24 @@ steps = []
 # The app should only take positive numbers
 
 # Function to compute the collatz conjecture for a given number
-def collatz(num):
+def collatz(number):
     global steps
-    # if num is even
-    if num % 2 == 0:
-        num = int(num / 2)
-        steps.append(num)
-        collatz(num)
-    # else if num is odd and not 1
-    elif (num % 2 == 1) and (num != 1):
-        num = int((3 * num) + 1)
-        steps.append(num)
-        collatz(num)
+    # if number is even
+    if number % 2 == 0:
+        number = int(number // 2)
+        steps.append(number)
+        collatz(number)
+    # else if number is odd and not 1
+    elif (number % 2 == 1) and (number != 1):
+        number = int((3 * number) + 1)
+        steps.append(number)
+        collatz(number)
     else:
-        # end if num reaches 1
+        # end if number reaches 1
+        print("The number has reached 1!")
         pass
     # return the number of steps
-    return(len(steps))
+    return len(steps)
 
 
 def main():
@@ -33,8 +34,10 @@ def main():
     # Call the collatz function with a value entered by the user
     number = input("Enter a positive number: ")
     result = collatz(int(number))
-    print(f"The collatz conjecture for the number you entered took: \
-        {result} calculations")
+    print(
+        f"The collatz conjecture for the number you entered took: \
+        {result} calculations"
+    )
     print(f"The collatz list is: {steps}")
 
 
